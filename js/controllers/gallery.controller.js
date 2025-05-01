bea986477d039281f0fe0de31e06978f8f7d5654
Yulia C <yulka1355@gmail.com>
2025-05-01 18:30:47 +0300

    Create gallery controller

---
+++ js/controllers/gallery.controller.js
@@ -0,0 +1,21 @@
+'use strict'
+
+function renderMemeGallery() {
+    const memeImgs = getMemeImgs()
+    console.log('memeImgs:', memeImgs)
+
+    const elMemeContainer = document.querySelector('.meme-container')
+    elMemeContainer.innerHTML =
+        `<label class="file-input" for="file-upload-input">
+                    <i class="fa-solid fa-upload"></i> Upload Image
+                    <input id="file-upload-input" type="file" name="meme" onchange="onMemeUpload(event)" style="display: none;"/>
+                </label>
+                ${memeImgs.map(meme => {
+            return `
+                    <article class="meme-img">
+                    <img onclick="onSelectMeme(this)" class="rounded-m meme-img" src="memes-imgs/${meme.id}.jpg" alt="meme">
+                    </article>`
+        }).join('')}`
+
+
+}
