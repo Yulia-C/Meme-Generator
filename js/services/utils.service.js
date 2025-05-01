610c4777e5d1f680c35370852b4de2c42bfa3c71
Yulia C <yulka1355@gmail.com>
2025-05-01 18:28:39 +0300

    Create getKewWord function in utils

--- js/services/utils.service.js
+++ js/services/utils.service.js
@@ -6,6 +6,23 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min //The maximum is inclusive and the minimum is inclusive 
 }
 
+function getKeyWords() {
+    const keyWords = ['funny', 'cartoon', 'coding', 'animal', 'cat', 'sad']
+    const wordCount = getRandomIntInclusive(1, 2)
+
+    const selectedWords = []
+    const usedIndexes = new Set()
+
+    while (selectedWords.length < wordCount) {
+        const randIdx = getRandomIntInclusive(0, keyWords.length - 1)
+        if (!usedIndexes.has(randIdx)) {
+            usedIndexes.add(randIdx)
+            selectedWords.push(keyWords[randIdx])
+        }
+    }
+    return selectedWords
+}
+
 function makeId(length = 6) {
     const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
     var txt = ''
@@ -14,3 +31,4 @@ function makeId(length = 6) {
     }
     return txt
 }
+
