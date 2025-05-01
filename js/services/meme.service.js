781d3da0a25b914dd8778e279378b512425ab343
Yulia C <yulka1355@gmail.com>
2025-05-01 18:33:17 +0300

    Redo meme service functions

--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,38 +1,90 @@
 'use strict'
 const STORAGE_KEY = 'memeDB'
 
-let gMemes = loadFromStorage(STORAGE_KEY) || []
+let gMeme = loadFromStorage(STORAGE_KEY) || []
 
-function getMemes() {
-    return gMemes
+
+
+var gImgs = _createMemeImgs(25)
+
+
+var gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function getMemeImgs(){
+    return gImgs
+}
+
+function getMeme() {
+    //  gMeme = {
+    //     selectedImgId: 2,
+    //     selectedLineIdx: 0,
+    //     lines: [
+    //         {
+    //             txt: 'I sometimes eat Falafel',
+    //             size: 20,
+    //             color: 'red',
+    //             align: 'center',
+    //             font: 'Arial',
+    //         }
+    //     ]
+    // }
+    return gMeme
 }
 
 function removeMeme(memeId) {
-    const memeIdx = gMemes.findIndex(meme => meme.id === memeId)
-    gMemes.splice(memeIdx, 1)
+    const memeIdx = gMeme.findIndex(meme => meme.id === memeId)
+    gMeme.splice(memeIdx, 1)
     _saveMemesToStorage()
 }
 
 function addMeme(data) {
     const meme = _createMeme(data)
-    gMemes.unshift(meme)
+    gMeme.unshift(meme)
     _saveMemesToStorage()
     return meme
 }
 
 function getMemeById(memeIdx) {
-    const meme = gMemes.find(meme => meme.id === memeIdx)
+    const meme = gMeme.find(meme => meme.id === memeIdx)
     return meme
 }
 
-function _createMeme(data) {
+function _createMeme(data){
     return {
-        id: makeId(6),
-        createdAt: Date.now(),
+        id:makeId(3),
         data
     }
+    // gMeme = {
+    //     selectedImgId: 2,
+    //     selectedLineIdx: 0,
+    //     lines: [
+    //         {
+    //             txt: 'I sometimes eat Falafel',
+    //             size: 20,
+    //             color: 'red',
+    //             align: 'center',
+    //             font: 'Arial',
+    //         }
+    //     ]
+    // }
+    
+   
+}
+
+function _createMemeImgs(count) {
+    const memes = []
+    for (var i = 0; i < count; i++) {
+        memes[i] = _createMemeImg(i + 1)
+    }
+    console.log('memes:', memes)
+    return memes
+}
+
+function _createMemeImg(id = 1) {
+    const meme = { id, url: `img/${id}.jpg`, keywords: getKeyWords() }
+    return meme
 }
 
 function _saveMemesToStorage() {
-    saveToStorage(STORAGE_KEY, gMemes)
+    saveToStorage(STORAGE_KEY, gMeme)
 }
